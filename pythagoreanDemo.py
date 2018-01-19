1b6a1890bfaf7fab73d383f778f929436b57fe2b
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-19 17:20:20 -0500

    Update pythagoreanDemo.py

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -1,1 +1,7 @@
-
+#Ray Tso
+#1/17/18
+#pythagoreanDemo.py
+a=float(input('What is the length of side 1? '))
+b=float(input('What is the length of side 2? ')) 
+print ('c=',a**2+b**2) 
+print ('hypotenuse', 'c'**0.5)
