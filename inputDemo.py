a53844b6515598ce56a36b9f3cc40a167b776590
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-19 15:56:16 -0500

    Updated from Brython Server: 1/19/2018 3:56:16 PM

--- inputDemo.py
+++ inputDemo.py
@@ -2,7 +2,7 @@
 #1/17/18
 #imputDemo.py- how to use imput
 
-name+ imput ('What is your name? ')
-print ('Hello, name)
-age=int(imput('How old are you" ))
+name= input ('What is your name? ')
+print ('Hello, name')
+age=int(input('How old are you' ))
 print(' You are', age+1, 'years old')
