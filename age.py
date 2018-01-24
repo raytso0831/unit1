bb89a41fda571ceac4667317719d821ca45221be
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-24 08:27:18 -0500

    Updated from Brython Server: 1/24/2018 8:27:17 AM

--- age.py
+++ age.py
@@ -2,5 +2,9 @@
 #1/22/18
 #shouter.py
 
-phrase= input('enter text')
-print(phrase.upper())
+phrase= input('enter first and last name')
+age=int(input('enter your age'))
+first,last=phrase.split()
+print(len(first))
+print(len(last))
+print(' Next year you will be', age+1)
