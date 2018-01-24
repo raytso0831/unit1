d22507580d92fcc7b98c2633ca30671753a17839
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-24 15:01:40 -0500

    Updated from Brython Server: 1/24/2018 3:01:39 PM

--- age.py
+++ age.py
@@ -2,9 +2,10 @@
 #1/22/18
 #age.py
 
-phrase= input('enter first and last name')
-age=int(input('enter your age'))
+phrase= input('enter first and last name:')
+age=int(input('enter your age:'))
 first,last=phrase.split()
-print(len(first))
-print(len(last))
+
+print('Number of letters in your first name:',len(first))
+print('Number of letters in your first name:',len(last))
 print(' Next year you will be', age+1)
