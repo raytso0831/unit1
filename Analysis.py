2937df921e6beb6b8d145af432b1fb8c3b9f6153
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-24 10:42:04 -0500

    Updated from Brython Server: 1/24/2018 10:42:03 AM

--- Analysis.py
+++ Analysis.py
@@ -1,1 +1,11 @@
+#Ray Tso
+#1/22/18
+#Analysis.py
+
+sentence= input('enter a sentence')
+age=int(input('enter your age'))
+first,last=phrase.split()
+print(len(first))
+print(len(last))
+print(' Next year you will be', age+1)
 
