54d90c957ef9b2ddbf76da2c41081c4dc4335f4e
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-25 07:59:37 -0500

    Updated from Brython Server: 1/25/2018 7:59:36 AM

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -1,9 +1,12 @@
 #Ray Tso
 #1/17/18
-#pythagoreanDemo.py
+#pythagoreanDemo.py  
+from math import sqrt
 a=float(input('What is the length of side 1? '))
 b=float(input('What is the length of side 2? ')) 
+
+c=a**2+b**2
 print ('c=',a**2+b**2) 
-print ('hypotenuse', from math import sqrt('c')
+print ('hypotenuse', sqrt(c))
 
 
