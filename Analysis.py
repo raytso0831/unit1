bda52a5b14e317b1d702ac494def0baa3131bd60
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-25 09:34:01 -0500

    Updated from Brython Server: 1/25/2018 9:34:00 AM

--- Analysis.py
+++ Analysis.py
@@ -2,11 +2,20 @@
 #1/22/18
 #Analysis.py
 
-sentence= input('enter a sentence')
+sentence= input('Enter a sentence:')
 
-age=int(input('enter your age'))
-first,last=phrase.split()
-print(len(first))
-print(len(last))
-print(' Next year you will be', age+1)
+space=sentence.count(' ')
 
+print(' Number of words in your sentence:', space+1)
+
+print('Number of characters in your sentence:',len(sentence))
+
+print('Number of letters in your sentence:', len(sentence)-space)
+
+search_of_char=input(' search for a character:')
+
+print('Number of character in your sentence', sentence.count(search_of_char))
+
+search_of_word=input(' search for a word:')
+
+print('Number of word in your sentence', sentence.count(search_of_word))
