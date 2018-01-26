d7bbdf80a72b47c7117335866c39042c5e414723
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-26 08:21:53 -0500

    Updated from Brython Server: 1/26/2018 8:21:51 AM

--- file quiz1.py
+++ file quiz1.py
@@ -1,1 +1,15 @@
+#Ray Tso
+#1/25/17
+#file quiz1.py
 
+print('Hello')
+
+name=input('What is your name')
+
+number=int(input('What is your favorite number'))
+
+print( name*number)
+
+sec_num=float(input('What is your second favorite number'))
+
+lucky_number= print(number**sec_num)
